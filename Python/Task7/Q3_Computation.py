93041de79cc5075a27349f88330821f8242a4e86
netra2127 <netra200021kcbdr@gmail.com>
2024-05-27 20:53:08 +0100

    Computation

--- Python/Task7/Q3_Computation.py
+++ Python/Task7/Q3_Computation.py
@@ -1,3 +1,19 @@
 """
 
+1. Create a Computation class with a default constructor (without parameters) allowing to perform various calculations on integers numbers.
+
+2. Create a method called Factorial() which allows to calculate the factorial of an integer n. Integer n as parameter for this method
+
+3. Create a method called naturalSum() allowing to calculate the sum of the first n integers 1 + 2 + 3 + .. + n. Integer n as parameter for this method.
+
+4. Create a method called testPrime() in the Calculation class to test the primality of a given integer n, n is Prime or Not? Integer n as parameter for this method.
+
+5. Create a method called testPrims() allowing to test if two numbers are prime between them. Two integers are prime to one another if they have only 1 as their common divisor. Eg. 4 and 9 are prime to each other.
+
+6. Create a tableMult() method which creates and displays the multiplication table of a given integer. Then create an allTablesMult() method to display all the integer multiplication tables 1, 2, 3, ..., 9.
+
+7. Create a static listDiv() method that gets all the divisors of a given integer on new list called Ldiv. Create another listDivPrim() method that gets all the prime divisors of a given integer.
 """
+
+
+
