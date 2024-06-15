64a2791b59d778f2eb73363a3eae08ea4d9839c2
netra2127 <netra200021kcbdr@gmail.com>
2024-06-15 12:40:23 +0100

    matplotlib

--- DataVisualization/matplotlib.py
+++ DataVisualization/matplotlib.py
@@ -83,3 +83,45 @@ plt.grid()
 plt.legend(loc='best') # best always takes where there is place in the graphs.
 plt.show()
 
+# scatter plot. 
+# Used for the Bivariate Analysis.
+# Only for the numerical vs numerical columns. 
+# Use case - Finding correlation.
+# plt.scatter simple function. 
+x = np.linspace(-10, 10, 50) # -10, +10, samma 50 points. 
+y = 10*x + np.random.randint(0,300,50)
+
+y
+
+# 
+plt.scatter(x,y)
+plt.xlabel("numerical inputs")
+plt.ylabel("prediction")
+
+# 
+df = pd.read_csv("/content/drive/MyDrive/Datasets_DataVisualization/batter.csv")
+df = df.head(50)
+df
+
+# 
+plt.scatter(df['avg'], df['strike_rate'], color='green')
+plt.title("Avg and strike rate analysis of Top 50 Batsman")
+plt.xlabel("Average")
+plt.ylabel("strike rate")
+
+# 
+# size
+import seaborn as sns
+tips = sns.load_dataset('tips')
+# tips
+# # This is slower techniques. 
+# s=tips['size']*20 represent the tyo particular customers ko sath ma aru kati Jana customers ako theeya tww, size le dekhaucha..
+plt.scatter(tips['total_bill'], tips['tip'], s=tips['size']*20)
+
+# 
+# scatterplot using plt.plot 
+# This is faster techniques. 
+plt.plot(tips['total_bill'], tips['tip'], 'o')
+
+# 
+
