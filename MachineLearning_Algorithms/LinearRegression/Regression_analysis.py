c1752ad25fdfe3305c71ca16914aae95dda64c0f
netr212 <netra200021kcbdr@gmail.com>
2025-03-12 10:34:40 +0000

    regression analysis

---
+++ MachineLearning_Algorithms/LinearRegression/Regression_analysis.py
@@ -0,0 +1,6 @@
+'''
+# Why ML Problems are a statistical Inference Problem ?
+Question Understanding: With the help of sample, we try to find the estimate value for the value <-- this is called statistical inference problem.
+Ans:- 
+
+'''
